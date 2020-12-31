5c58212ca6d07a88ad2af37006edb44aa2bf4c8b
舘一輝 <kanikki@kanikkinoMacBook-Air.local>
2020-12-31 12:42:52 +0900

    djangoの仕様で省略されたテンプレート名を表記

--- comments/views.py
+++ comments/views.py
@@ -13,34 +13,39 @@ from .models import Comment
  
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the comments index.")
- 
+
+#template_name省略できる　https://qiita.com/igm/items/24e9c5431d0e8fba0700
 class CommentIndexView(ListView):
   model = Comment
+  template_name = 'comments/comment_list.html'
   queryset = Comment.objects.order_by('-updated_at')
   paginate_by = 10
 
 class ShowCommentView(DetailView):
   model = Comment
+  template_name = 'comments/comment_detail.html'
 
 class CreateCommentView(CreateView):
-    model = Comment
-    form_class = CommentForm
-    success_url = reverse_lazy('comments:index')
+  model = Comment
+  template_name = 'comments/comment_form.html'
+  form_class = CommentForm
+  success_url = reverse_lazy('comments:index')
 
-    def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context['page_title'] = 'コメントの投稿'
-      context['form_name'] = 'コメントの投稿'
-      context['button_label'] = 'コメントを投稿する'
-      return context
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['page_title'] = 'コメントの投稿'
+    context['form_name'] = 'コメントの投稿'
+    context['button_label'] = 'コメントを投稿する'
+    return context
 
-    def form_valid(self, form):
-      self.object = comment = form.save()
-      messages.success(self.request, 'コメントを投稿しました')
-      return redirect(self.get_success_url())
+  def form_valid(self, form):
+    self.object = comment = form.save()
+    messages.success(self.request, 'コメントを投稿しました')
+    return redirect(self.get_success_url())
 
 class UpdateCommentView(UpdateView):
     model = Comment
+    template_name = 'comments/comment_form.html'
     form_class = CommentForm
     success_url = reverse_lazy('comments:index')
 
@@ -60,7 +65,7 @@ class UpdateCommentView(UpdateView):
 class DeleteCommentView(DeleteView):
     model = Comment
     success_url = reverse_lazy('comments:index')
-
+    template_name = 'comments/comment_confirm_delete.html'
     def delete(self, request, *args, **kwargs):
       self.object = comment = self.get_object()
       comment.delete()
