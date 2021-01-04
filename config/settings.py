a8716aa6516c951975604d4486aec0bd24f5d4a7
tate12 <tate@sbicb.co.jp>
2021-01-04 13:29:40 +0900

    stocksテーブル定義

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'stocks.apps.StocksConfig',
     'comments.apps.CommentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#DB定義
+
+class Stock(models.Model):
+        class Meta:
+            db_table = 'stock'
+        ticker = models.CharField(max_length=20)
+        price = models.IntegerField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
